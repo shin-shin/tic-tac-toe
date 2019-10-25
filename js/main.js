a3a965403878ea2d3547a1671e6afff38e1bf459
Irene Tokareva <longhorn09@gmail.com>
2019-10-25 16:53:03 -0500

    test eventListener on board

--- js/main.js
+++ js/main.js
@@ -5,26 +5,40 @@ const EMPTY = 0;
 const WIN_NUM = 3;
 
 //*----- app's state (variables) -----
-let board, resetGameBtn, xTurn, oTurn;
+let board, cleanBoard, playBoard, resetGameBtn, xTurn, oTurn;
 
 //*----- cached element references ---
 board = document.getElementById("board");
 resetGameBtn = document.getElementById("resetBtn");
+
 //*----- event listeners -------------
 board.addEventListener("click", makeMove);
-resetGameBtn.addEventListener("click", resetGame)
-//*----- functions -------------------
-function makeMove(board){
-    console.log("move");
-}
-function resetGame(){
-    console.log("reset");
-}
-function init(){
+resetGameBtn.addEventListener("click", resetGame);
 
+//*----- functions -------------------
+init();
+
+function makeMove(event){
+    //test if it works
+    event.target.style.backgroundColor = "red";
+    //playBoard = cleanBoard.push(event.target);
+    console.log("move " + event.target);
 }
+
 function winCheck(){
 
 }
 
-console.log("test");
+function resetGame(event){
+    console.log("reset");
+}
+
+function render(){
+
+}
+function init(){
+    cleanBoard = [[0,0,0],[0,0,0],[0,0,0]];
+
+}
+
+console.log("hello from JS");
