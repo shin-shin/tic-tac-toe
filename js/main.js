8e2a2d58a568e8c454ae179df60ba70069acaa9c
Irene Tokareva <longhorn09@gmail.com>
2019-10-25 16:32:54 -0500

    add event listener to reset button

--- js/main.js
+++ js/main.js
@@ -5,17 +5,23 @@ const EMPTY = 0;
 const WIN_NUM = 3;
 
 //*----- app's state (variables) -----
-let board, resetGame, xTurn, oTurn;
+let board, resetGameBtn, xTurn, oTurn;
 
 //*----- cached element references ---
 board = document.getElementById("board");
-
+resetGameBtn = document.getElementById("resetBtn");
 //*----- event listeners -------------
 board.addEventListener("click", makeMove);
-
+resetGameBtn.addEventListener("click", resetGame)
 //*----- functions -------------------
 function makeMove(board){
     console.log("move");
+}
+function resetGame(){
+    console.log("reset");
+}
+function init(){
+
 }
 function winCheck(){
 
