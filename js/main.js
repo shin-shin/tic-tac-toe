7dda7435a783b347ad418ca2ff307d09169d69b9
Irene Tokareva <longhorn09@gmail.com>
2019-10-25 16:15:01 -0500

    add event listener to board

--- js/main.js
+++ js/main.js
@@ -3,13 +3,20 @@ const XBOX = 1;
 const OBOX = -1;
 const EMPTY = 0;
 const WIN_NUM = 3;
+
 //*----- app's state (variables) -----
-const board = [[],[],[]];
+let board, resetGame, xTurn, oTurn;
+
 //*----- cached element references ---
+board = document.getElementById("board");
 
 //*----- event listeners -------------
+board.addEventListener("click", makeMove);
 
 //*----- functions -------------------
+function makeMove(board){
+    console.log("move");
+}
 function winCheck(){
 
 }
