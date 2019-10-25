50c7fd53ebf3db32951e193cda31ab3acf421320
Irene Tokareva <longhorn09@gmail.com>
2019-10-25 12:18:46 -0500

    add HTML and CSS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//*----- constants -------------------
+
+//*----- app's state (variables) -----
+
+//*----- cached element references ---
+
+//*----- event listeners -------------
+
+//*----- functions -------------------
+console.log("test");
