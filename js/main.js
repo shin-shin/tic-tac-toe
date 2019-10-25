dbd62ad184301c7f0abf24c445e9dacfbd8aaa81
Irene Tokareva <longhorn09@gmail.com>
2019-10-25 15:13:29 -0500

    update HTML file with turn structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 //*----- constants -------------------
-
+const XBOX = 1;
+const OBOX = -1;
+const EMPTY = 0;
+const WIN_NUM = 3;
 //*----- app's state (variables) -----
-
+const board = [[],[],[]];
 //*----- cached element references ---
 
 //*----- event listeners -------------
 
 //*----- functions -------------------
+function winCheck(){
+
+}
+
 console.log("test");
