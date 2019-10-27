fd73e3991324ea4591ca6873927a3689af5b7997
Irene Tokareva <longhorn09@gmail.com>
2019-10-27 16:16:43 -0500

    add winCheck logic for rows

--- js/main.js
+++ js/main.js
@@ -8,53 +8,116 @@ const GRID13 = 13;
 
 
 //*----- app's state (variables) -----
-let board, playBoard, boxNum, resetGameBtn, xTurn, oTurn;
+let board, playboard, boxIdx, resetGameBtn, turn, turnBoxX, turnBoxO;
 
 //*----- cached element references ---
 board = document.getElementById("board");
 resetGameBtn = document.getElementById("resetBtn");
+turnBoxX = document.querySelector("#turnBox .xBox");
+turnBoxO = document.querySelector("#turnBox .oBox");
 //*----- event listeners -------------
 board.addEventListener("click", makeMove);
 resetGameBtn.addEventListener("click", resetGame);
 
 //*----- functions -------------------
 init();
+function build() {
+    for (var i = 0; i < Math.pow(GRID, 2); i++) {
+        board.innerHTML += "<div></div>";
+    }
+}
 
-function makeMove(event){
+function makeMove(e) {
     let move = event.target;
-    let boxNum = playBoard.indexOf(move);
-    move.textContent = "0"
-    console.log(boxNum);
-    //array of (#board children elements numbered) method indexOf(event.target)
-    //for loop through #board 
-    //test if it works
-    move.style.backgroundColor = "#FE506D";
-    //move.textContent = "x";
-    //playBoard.push(move);
-    //console.log("move " + move);
-    //console.log(playBoard);
+    //console.log(`move in makeMove 1 ${move}`);
+    let boxIdx = playboard.indexOf(move);
+    //console.log(`boxIdx ${boxIdx}`);
+
+    if (boxIdx == -1) {
+        //the box was already played
+        return;
+    }
+
+    //replace boxIdx position in array with -1/1
+    playboard[boxIdx] = turn;
+    turn > 0 ? move.className = "xBox" : move.className = "oBox";
+
+    move.textContent = (turn === 1 ? "x" : "o")
+    console.log("turn is " + turn);
+    turn = turn * -1;
+
+
+
+
+    winCheck();
+    render(move);
+
 }
 
-function winCheck(){
+function winCheck() {
+    //console.log(`playboard.length is ${playboard.length}`);
+    console.log(`firstBox is ${playboard[0]}`);
+    console.log(`secondBox is ${playboard[1]}`);
+
+    for (let i = 0; i < playboard.length; i += GRID) {
+        console.log(`i: ${i}, i + GRID: ${i+GRID}`);
+        let sl = playboard.slice(i, i + GRID);
+
+        let sum = sl.reduce(function (acc, curr) {
+            console.log(`curr is ${curr}`);
+            if (Number.isInteger(curr)) {
+                console.log(`CURR IS A NUMBER: ${curr}`);
+                return acc + curr;
+            } else {
+                console.log(`not integer`);
+                return acc;
+            }
+        }, 0)
+
+        let aSum = Math.abs(sum)
+        console.log(`Sum is ${aSum}`)
+        if (Math.abs(sum) === 3) {
+            console.log("win")
+            return;
+        }
+        //console.log(`slice length is ${sl.length}`);
+        //console.log(`slice is ${sl}`);
+    }
+
 
 }
 
-function resetGame(event){
+function resetGame(e) {
+    board.innerHTML = "";
+
     init();
     console.log("reset");
 }
 
-function render(){
+function render(m) {
 
-}
-function init(){
-    // playBoard = new Array();
-    for(var i = 0; i < Math.pow(GRID, 2); i++){
-        board.innerHTML += "<div class=\"zero\"></div";
+    //display the X or O turn
+    if (turn === 1) {
+        turnBoxO.className = "oBox";
+        turnBoxX.className = "xBox active";
+    } else {
+        turnBoxX.className = "xBox";
+        turnBoxO.className = "oBox active";
     }
-    playBoard = Array.from(document.getElementById("board").children);
 
-    console.log(playBoard);
+    //render win
+
+}
+function init() {
+    turn = 1;
+    build();
+
+    playboard = Array.from(document.getElementById("board").children);
+    // playboard.forEach(function(el, idx){
+    //     playboard[idx] = 0;
+    // })
+    //playboard = new Array(9).fill(0);
+
+    render();
 }
 
-console.log("hello from JS");
